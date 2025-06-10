4af1152838297e2646dfcc020e8386f435c5eed3
LeoHellmig <220383@buas.nl>
2025-06-10 10:48:29 +0200

    bugfix !contains()

--- dungeonerator/dungeonerator.cpp
+++ dungeonerator/dungeonerator.cpp
@@ -218,7 +218,7 @@ void Dungeon::Generate() {
 			{
 				nrOfSearches++;
 				std::uint32_t key = intDistribution(gen);
-				if (key < min && mstKeySet.contains(vert.mConnections[j]))
+				if (key < min && !mstKeySet.contains(vert.mConnections[j]))
 				{
 					a = mstSet[i];
 					b = vert.mConnections[j];
